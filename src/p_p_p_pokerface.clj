6febbcb566a1ed73be8c98bb9aa0eb8f030d8929
Cameron Candelori <cameron.nigro@gmail.com>
2025-05-03 14:38:17 -0400

    learn poker hands

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,65 @@
 (ns p-p-p-pokerface)
 
+(defn hand [a-hand]
+  a-hand)
+
+(defn card [a-card]
+  a-card)
+
 (defn rank [card]
-  nil)
+  (let [[first _] card]
+    (if (Character/isDigit first)
+      (Integer/valueOf (str first))
+      (get {\T 10, \J 11, \Q 12, \K 13 \A 14} first))))
 
 (defn suit [card]
-  nil)
+  (let [[_ second] card] (str second)))
 
 (defn pair? [hand]
-  nil)
+  (< (count (set (map rank hand)))
+     (count hand)))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (>= (apply max (vals (frequencies (map rank hand))))
+      3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (= (apply max (vals (frequencies (map rank hand))))
+     4))
 
 (defn flush? [hand]
-  nil)
+  (= 1 (count (set (map suit hand)))))
 
 (defn full-house? [hand]
-  nil)
+  (= (sort (vals (frequencies (map rank hand))))
+     [2 3]))
 
 (defn two-pairs? [hand]
-  nil)
+  (= (sort (vals (frequencies (map rank hand))))
+     [1 2 2]))
 
 (defn straight? [hand]
-  nil)
+  (let [ranks (sort (map rank hand))
+        low (first ranks)
+        target (range low (+ low 5))
+        ace-low? (= ranks [2 3 4 5 14])
+        ranks-ace (sort (map #(if (= % 14) 1 %) ranks))] 
+    (or 
+     ace-low?
+     (= ranks target)
+     (= ranks-ace target))))
 
-(defn straight-flush? [hand]
-  nil)
+  (defn straight-flush? [hand]
+    (and (straight? hand) (flush? hand)))
 
-(defn value [hand]
-  nil)
+  (defn value [hand]
+    (cond
+      (straight-flush? hand) 8
+      (four-of-a-kind? hand) 7
+      (full-house? hand) 6
+      (flush? hand) 5
+      (straight? hand) 4
+      (three-of-a-kind? hand) 3
+      (two-pairs? hand) 2
+      (pair? hand) 1
+      :else 0))
